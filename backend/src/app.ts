31cff3e2821182a65e465457683a8fc7cae64119
Rufio244 <thanvas9991@gmail.com>
2026-04-19 05:16:30 +0700

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const { v4: uuidv4 } = require('uuid');
 
+app.use(express.json());
+
+let rooms = {};
+let wallets = {};
+
+app.get('/', (req, res) => {
+  res.send('TUI GAME API RUNNING');
+});
