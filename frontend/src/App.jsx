bacfef8a6f584237e470d3c7c9b7df0d2bf76d6f
Rufio244 <thanvas9991@gmail.com>
2026-04-19 05:17:16 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,41 @@
+import { useState } from "react";
+import axios from "axios";
 
+function App() {
+  const [round, setRound] = useState(null);
+  const [cards, setCards] = useState({});
+
+  const startGame = async () => {
+    const res = await axios.post("http://localhost:3000/game/start");
+    setRound(res.data);
+  };
+
+  const deal = async () => {
+    const res = await axios.post("http://localhost:3000/game/deal", {
+      roundId: round.roundId
+    });
+    setCards(res.data);
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>🎮 TUI GAME</h1>
+
+      <button onClick={startGame}>Start Game</button>
+      <button onClick={deal}>Deal Cards</button>
+
+      <div style={{ marginTop: 20 }}>
+        {Object.keys(cards).map(seat => (
+          <div key={seat}>
+            <h3>Seat {seat}</h3>
+            {cards[seat].map((c, i) => (
+              <button key={i}>{c}</button>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
