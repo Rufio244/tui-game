5de47ac775c7dbe82c64e07b39974ed6cdec578c
Rufio244 <thanvas9991@gmail.com>
2026-04-19 02:38:46 +0700

    Update deck.js

--- server/deck.js
+++ server/deck.js
@@ -1,1 +1,8 @@
+export const deck = [
+  'A','A','2','2','3','3','4','4',
+  '5','5','6','6','7','7','8','8','K','K'
+];
 
+export function shuffle(cards) {
+  return [...cards].sort(() => Math.random() - 0.5);
+}
